1c394f467b9705c92bb131630e78de0617f5fa9f
Holger Rünkaru <hrunkaru@outlook.com>
2022-03-25 10:17:25 +0200

    Get certinfo to csv

--- Get-LOTLInfo.ps1
+++ Get-LOTLInfo.ps1
@@ -1,1 +1,20 @@
-#powershell code will be added here
+$pathToPathsCSV = "LOLBAS_filepaths.csv"
+$outfile = "LOTLInfo_$(Get-Date -UFormat "%Y-%m-%d_%H-%m-%S").csv"
+
+$data = import-csv $pathToPathsCSV
+$outputdata = @()
+
+# Include both OSBinaries and OtherMSBinaries, exclude OSLibraries and OSScripts
+$data | Where-Object Category -like "*Binaries" | ForEach-Object {
+    
+    # for each file gather signature data with:
+    # Select-Object -Property statusmessage, signaturetype, isosbinary -ExpandProperty SignerCertificate | Select-Object statusmessage, signaturetype, isosbinary, issuer, subject
+
+    if (Test-Path -Path $_.Path){
+        
+         $outputdata += Get-AuthenticodeSignature $_.Path | Select-Object -Property statusmessage, signaturetype, isosbinary -ExpandProperty SignerCertificate | Select-Object statusmessage, signaturetype, isosbinary, issuer, subject
+    }
+}
+
+$outputdata | Export-Csv -Path $outfile -NoTypeInformation
+
