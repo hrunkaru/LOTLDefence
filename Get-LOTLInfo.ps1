eca72e1241cca2d3d3ebe41eda11d3af1425a17e
Holger Rünkaru <hrunkaru@outlook.com>
2022-03-25 11:40:32 +0200

    Added parsing for not found files

--- Get-LOTLInfo.ps1
+++ Get-LOTLInfo.ps1
@@ -3,18 +3,35 @@ $outfile = "LOTLInfo_$(Get-Date -UFormat "%Y-%m-%d_%H-%M-%S").csv"
 
 $data = import-csv $pathToPathsCSV
 $outputdata = @()
+$countTestPath = 0
+
+write-debug "Total items found from input CSV file: $($data | Measure-Object | Select-Object -ExpandProperty count)"
 
 # Include both OSBinaries and OtherMSBinaries, exclude OSLibraries and OSScripts
+write-debug "Total items after filtering by type: $($data | Where-Object Category -like "*Binaries" | Measure-Object | Select-Object -ExpandProperty count)"
 $data | Where-Object Category -like "*Binaries" | ForEach-Object {
     
-    # for each file gather signature data with:
-    # Select-Object -Property statusmessage, signaturetype, isosbinary -ExpandProperty SignerCertificate | Select-Object statusmessage, signaturetype, isosbinary, issuer, subject
-
     if (Test-Path -Path $_.Path){
-        
-         $outputdata += Get-AuthenticodeSignature $_.Path | Select-Object -Property statusmessage, signaturetype, isosbinary -ExpandProperty SignerCertificate | Select-Object statusmessage, signaturetype, isosbinary, issuer, subject
+        $countTestPath += 1
+
+        $item = $(Get-AuthenticodeSignature $_.Path |
+         Select-Object -Property * -ExpandProperty SignerCertificate | 
+         Select-Object statusmessage, signaturetype, isosbinary, issuer, subject, Name, Path
+         )
+         $item.Name = $_.Name
+         $item.Path = $_.Path
+         $outputdata += $item
+
+    }
+    else {
+        $item = $_ | Select-Object statusmessage, signaturetype, isosbinary, issuer, subject, Name, Path
+        $item.StatusMessage = "File not found"
+        $outputdata += $item
     }
 }
 
+write-debug "Total existing paths in the system: $countTestPath"
 $outputdata | Export-Csv -Path $outfile -NoTypeInformation
+write-debug "Total items in output data: $($outputdata | Measure-Object | Select-Object -expandproperty count)"
 
+ 
